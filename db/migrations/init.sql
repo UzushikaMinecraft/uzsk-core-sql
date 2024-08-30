f21cb252991ac8f65ae91246944e2e313248edcd
iamtakagi <github@iamtakagi.net>
2024-08-30 21:21:37 +0900

    simply

--- db/migrations/init.sql
+++ db/migrations/init.sql
@@ -1,10 +1,10 @@
 CREATE TABLE Players (
     ID INT PRIMARY KEY AUTO_INCREMENT,
     UUID CHAR(36) NOT NULL UNIQUE,
-    InitialLoginDateTime DATETIME NOT NULL,
-    LastLoginDateTime DATETIME,
+    InitialLoginDate DATETIME NOT NULL,
+    LastLoginDate DATETIME,
     PlayTime INT DEFAULT 0,
-    ExperiencePoints INT DEFAULT 0, 
+    Experiences INT DEFAULT 0, 
     Currency INT DEFAULT 0, 
     TotalBuildBlocks INT DEFAULT 0,
     TotalDestroyBlocks INT DEFAULT 0, 
