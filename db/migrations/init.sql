7a230e4947145b1b6a3dee286124ee7a196017e8
iamtakagi <github@iamtakagi.net>
2024-09-05 18:36:13 +0900

    feat: (table) bedrock

--- db/migrations/init.sql
+++ db/migrations/init.sql
@@ -2,11 +2,17 @@ CREATE TABLE `profile` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `uuid` CHAR(36) NOT NULL UNIQUE,
     `initial_login_date` TIMESTAMP NOT NULL,
-    `last_login_date` TIMESTAMP,
-    `total_play_time` BIGINT,
+    `last_login_date` TIMESTAMP NOT NULL,
+    `total_play_time` BIGINT NOT NULL,
     `experience` FLOAT DEFAULT 0.0, 
     `currency` INT DEFAULT 0, 
     `total_build_blocks` INT DEFAULT 0,
     `total_destroy_blocks` INT DEFAULT 0, 
     `total_mob_kills` INT DEFAULT 0
+);
+
+CREATE TABLE `bedrock` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `fuid` CHAR(36) NOT NULL,
+    `xuid` CHAR(36) NOT NULL,
 );
